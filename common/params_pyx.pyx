0bcf0cb3d739e58b9f42b25d75221b9f596f7cd6
myhdh10 <myhdh10@gmail.com>
2020-11-16 15:51:00 +0900

    revert unsupported grey, whitepanda

--- common/params_pyx.pyx
+++ common/params_pyx.pyx
@@ -72,7 +72,6 @@ keys = {
   b"Offroad_IsTakingSnapshot": [TxType.CLEAR_ON_MANAGER_START],
   b"Offroad_NeosUpdate": [TxType.CLEAR_ON_MANAGER_START],
   b"Offroad_UpdateFailed": [TxType.CLEAR_ON_MANAGER_START],
-  b"Offroad_HardwareUnsupported": [TxType.CLEAR_ON_MANAGER_START],
 }
 
 def ensure_bytes(v):
--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -9,11 +9,12 @@ from selfdrive.swaglog import cloudlog
 import cereal.messaging as messaging
 from selfdrive.car import gen_empty_fingerprint
 
-from cereal import car
+from cereal import car, log
 EventName = car.CarEvent.EventName
+HwType = log.HealthData.Hwtype
 
 
-def get_startup_event(car_recognized, controller_available):
+def get_startup_event(car_recognized, controller_available, hw_type):
   #if comma_remote and tested_branch:
   #  event = EventName.startup
   #else:
@@ -25,6 +26,8 @@ def get_startup_event(car_recognized, controller_available):
     event = EventName.startupNoCar
   elif car_recognized and not controller_available:
     event = EventName.startupNoControl
+  elif hw_type == HwType.greyPanda:
+    event = EventName.startupGreyPanda
   return event
 
 
@@ -83,11 +86,11 @@ def only_toyota_left(candidate_cars):
 
 
 # **** for use live only ****
-def fingerprint(logcan, sendcan):
+def fingerprint(logcan, sendcan, has_relay):
   fixed_fingerprint = os.environ.get('FINGERPRINT', "")
   skip_fw_query = os.environ.get('SKIP_FW_QUERY', False)
 
-  if not fixed_fingerprint and not skip_fw_query:
+  if has_relay and not fixed_fingerprint and not skip_fw_query:
     # Vin query only reliably works thorugh OBDII
     bus = 1
 
@@ -169,15 +172,15 @@ def fingerprint(logcan, sendcan):
   return car_fingerprint, finger, vin, car_fw, source
 
 
-def get_car(logcan, sendcan):
-  candidate, fingerprints, vin, car_fw, source = fingerprint(logcan, sendcan)
+def get_car(logcan, sendcan, has_relay=False):
+  candidate, fingerprints, vin, car_fw, source = fingerprint(logcan, sendcan, has_relay)
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "HYUNDAI KONA EV 2019"
 
   CarInterface, CarController, CarState = interfaces[candidate]
-  car_params = CarInterface.get_params(candidate, fingerprints, car_fw)
+  car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
   car_params.carVin = vin
   car_params.carFw = car_fw
   car_params.fingerprintSource = source
