0e5269ef37290bc26b503cd97d8a75beafb32eb8
myhdh10 <myhdh10@gmail.com>
2020-11-17 05:24:49 +0900

    modify wrong typo

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -11,7 +11,7 @@ from selfdrive.car import gen_empty_fingerprint
 
 from cereal import car, log
 EventName = car.CarEvent.EventName
-HwType = log.HealthData.Hwtype
+HwType = log.HealthData.HwType
 
 
 def get_startup_event(car_recognized, controller_available, hw_type):
